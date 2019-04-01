d41d8cbab04a74fa87df0539908ea789dfef3eff
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2019-04-01 21:24:54 +0530

    Create ex4_table.py

---
+++ ex4_table.py
@@ -0,0 +1,13 @@
+## Date : 01-APR-2019
+## TaBLE
+##
+##
+##
+##
+
+
+var1 = input("Enter the number to print table")
+i = int (1)
+while (i < 11 ):
+    print (var1, "*", i, "===>", int (var1)*int(i) )
+    i = i + 1
