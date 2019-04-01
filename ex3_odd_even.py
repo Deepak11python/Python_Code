2997d447a5e7c5a87331618aa49faf3cc93f408d
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2019-04-01 21:16:31 +0530

    Create ex3_odd_even.py

---
+++ ex3_odd_even.py
@@ -0,0 +1,15 @@
+## Date : 01-APR-2019
+## ODD/EVEN
+##
+##
+##
+##
+
+var1 = int(input("Enter the number of your choice:"))
+
+mod11 = var1 % 2
+
+if ( mod11 == 0 ):
+    print ("Number is even")
+else:
+    print ("Number is odd")
