a429bf62411dde0bab2eec09246739bd21b39387
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2019-04-01 20:51:11 +0530

    Create ex1_variable.py

---
+++ ex1_variable.py
@@ -0,0 +1,12 @@
+## Date : 01-APR-2019
+## Variable, accept input from users
+##
+##
+##
+##
+Name = str(input("Enter your name:")) 
+Age = int(input("Enter your age:"))
+Ht = int(input("Enter your Height in cms:"))
+wt  = int(input("Enter your Weight in KG:"))
+
+print ("Welcome Mr.", Name, "Your age is ", Age, "weight,height are", wt,Ht)
