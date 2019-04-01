00b4f24f251c8154a2df18390bc17e6e3dfe54e8
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2019-04-01 21:08:34 +0530

    Create ex2_operator.py

---
+++ ex2_operator.py
@@ -0,0 +1,25 @@
+## Date : 01-APR-2019
+## Operators
+##
+##
+##
+##
+
+
+
+var1 = int(input("Enter the 1st Number:"))
+var2 = int(input("Enter the 2nd Number:"))
+
+sum11 = var1 + var2
+sub11 = var1 - var2
+div11 = var1 / var2
+mulp11 = var1 * var2
+mod11 = var1 % var2
+floor11 = var1 // var2
+
+print ("\nAddition :", sum11 )
+print ("\nSubstraction :", sub11 ) 
+print ("\nDivision :", div11 ) 
+print ("\nMultiplcationAddition :", mulp11 ) 
+print ("\nModule :", mod11 ) 
+print ("\nFloor divison :", floor11 ) 
